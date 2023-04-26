7a7b8f069ef57e79a47abe4443e6426c05a90a6e
Baptiste PERRIN <baptiste.perrin@orange.com>
2023-04-26 11:33:53 +0200

    Correction self signed (erreur précédente commit)
    Signed-off-by: Baptiste PERRIN <baptiste.perrin@orange.com>

--- src/Controllers/AuthController.ts
+++ src/Controllers/AuthController.ts
@@ -197,10 +197,13 @@ router.post(
     const mailer = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: parseInt(process.env.SMTP_PORT as string),
-      secure: process.env.SECURE_MAIL === 'true',
+      secure: (process.env.SMTP_USE_SSL ?? 'true') === 'true',
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
+      },
+      tls: {
+        rejectUnauthorized: (process.env.SMTP_CHECK_CERT ?? 'true') === 'true',
       },
     });
 
